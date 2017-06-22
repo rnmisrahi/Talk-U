85199ffb44d662ac616944e8ef590350e711178e
Ruben Misrahi <rnmisrahi@gmail.com>
2017-06-22 12:23:17 +0300

    stam

---
+++ TT/TT/Data/DBInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TT.Data;
+using TT.Models;
+
+namespace TT.Data
+{
+    public class DBInitializer
+    {
+
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Tracks.Any())
+            {
+                return;
+            }
+
+            var Tracks = new Track[]
+            {
+                new Track{Name="Chief Scientist"},
+                new Track {Name="Management"},
+                new Track{Name="Marketing"},
+                new Track{Name="Programming"},
+                new Track{Name="Other"}
+            };
+            foreach (Track track in Tracks)
+            {
+                context.Tracks.Add(track);
+            }
+            context.SaveChanges();
+        }
+
+    }
+}
